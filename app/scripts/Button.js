2c94e70d911d3b61f800bee34294e1035b25a722
madmed <medbelh@gmail.com>
2013-05-23 23:05:19 +0100

    linting

--- app/scripts/Button.js
+++ app/scripts/Button.js
@@ -1,23 +1,23 @@
+/*global createjs, update:true*/
+
 (function() {
+    'use strict';
 
     var Button = function(label, color) {
-      this.initialize(label, color);
-    }
+        this.initialize(label, color);
+    };
     var p = Button.prototype = new createjs.Container(); // inherit from Container
-
-    p.label;
-    p.background;
 
     p.Container_initialize = p.initialize;
     p.initialize = function(label, color) {
         this.Container_initialize();
         
         this.label = label;
-        if (!color) { color = "#CCC"; }
+        if (!color) { color = '#CCC'; }
         
-        var text = new createjs.Text(label, "20px Arial", "#000");
-        text.textBaseline = "center";
-        text.textAlign = "center";
+        var text = new createjs.Text(label, '20px Arial', '#000');
+        text.textBaseline = 'center';
+        text.textAlign = 'center';
         
         var width = text.getMeasuredWidth()+30;
         var height = text.getMeasuredHeight()+20;
@@ -26,22 +26,19 @@
         this.background.graphics.beginFill(color).drawRoundRect(0,0,width,height,10);
         this.background.regX = width/2;
         this.background.regY = height/2;
-
-        
-
         
         this.addChild(this.background,text);
-    }
-
-    p.onPress = pressHandler;
+    };
 
     function pressHandler(e){
         e.onMouseMove = function(ev){
             e.target.x = ev.stageX;
             e.target.y = ev.stageY;
             update = true;
-        }
+        };
     }
+
+    p.onPress = pressHandler;
 
     window.Button = Button;
 }());
--- app/scripts/Connection.js
+++ app/scripts/Connection.js
@@ -1,15 +1,13 @@
+/*global createjs*/
+
 (function() {
+    'use strict';
 
     var Connection = function(button1, button2, color) {
-      this.initialize(button1, button2, color);
-    }
+        this.initialize(button1, button2, color);
+    };
 
     var c = Connection.prototype = new createjs.Container(); // inherit from Container
-
-    c.line;
-    c.button1;
-    c.button2;
-    c.color;
 
     c.Container_initialize = c.initialize;
     c.initialize = function(button1, button2, color) {
@@ -18,15 +16,16 @@
         this.button2 = button2;
         this.color = color;
         this.line = new createjs.Shape();
-        this.drawLine();  
+        this.drawLine();
         this.addChild(this.line);
-    }
+    };
 
     c.drawLine = function() {
         this.line.graphics.clear().beginStroke(this.color)
             .moveTo(this.button1.x, this.button1.y)
-            .bezierCurveTo(this.button1.x-100, this.button1.y-100, 
-                this.button2.x+100, this.button2.y+100,
+            .bezierCurveTo(this.button1.x-100, this.button1.y-100,
+                this.button2.x+100,
+                this.button2.y+100,
                 this.button2.x, this.button2.y)
             .endStroke();
     };
